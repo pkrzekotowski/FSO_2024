f1634c735cd50cc3bf0cfd20fafec069e3e956cb
pkrzekotowski <piotr.krzekotowski79@gmail.com>
2024-03-30 17:15:14 +0100

    completed exercise1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -26,15 +26,39 @@ const Counter = ({ counter, text }) => {
   )
 }
 
+const PositivePercentage = ({ counter, text }) => {
+  return (
+    <div>
+      {text} {counter} %
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const handleGoodVote = () => setGood(good + 1)
-  const handleNeutralVote = () => setNeutral(neutral + 1)
-  const handleBadVote = () => setBad(bad + 1)
+  const average = ((good * 1 - bad * -1 + neutral * 0)/ all) || 0
+  const positive = ((good / all) * 100) || 0
+
+
+  const handleGoodVote = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+
+  const handleBadVote = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
+
+  const handleNeutralVote = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
 
   return (
     <div>
@@ -46,6 +70,9 @@ const App = () => {
       <Counter counter={good} text={'good'} />
       <Counter counter={neutral} text={'neutral'} />
       <Counter counter={bad} text={'bad'} />
+      <Counter counter={all} text={'all'} />
+      <Counter counter={average} text={'average'} />
+      <PositivePercentage counter={positive} text={'positive'} />
     </div>
   )
 }
